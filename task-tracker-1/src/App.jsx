a0510a12d5a66594d9e3c0b273f57b8f25250da5
Lindamell Abadoo <linmell2010@gmail.com>
2023-04-20 14:53:41 +0000

    vite plus react config

---
+++ task-tracker-1/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+import './scss/main.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+
+    </div>
+  )
+}
+
+export default App
